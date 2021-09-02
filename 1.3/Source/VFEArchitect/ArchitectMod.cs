5b4e3e53aa7ad1d77bce67822899fc091ea1d10f
Joshua <legodude17@users.noreply.github.com>
2021-09-02 10:13:04 -0700

    Most C# done

--- 1.3/Source/VFEArchitect/ArchitectMod.cs
+++ 1.3/Source/VFEArchitect/ArchitectMod.cs
@@ -6,15 +6,48 @@ namespace VFEArchitect
 {
     public class ArchitectMod : Mod
     {
+        public static AccessTools.FieldRef<Designator_Build, BuildableDef> GetEntDef = AccessTools.FieldRefAccess<Designator_Build, BuildableDef>("entDef");
+
         public ArchitectMod(ModContentPack content) : base(content)
         {
             var harm = new Harmony("vanillaexpanded.furniture.architect");
             harm.Patch(AccessTools.Method(typeof(SectionLayer_BridgeProps), "ShouldDrawPropsBelow"), postfix: new HarmonyMethod(typeof(ArchitectMod), nameof(IsVanillaBridge)));
+            harm.Patch(AccessTools.Method(typeof(CostListCalculator), nameof(CostListCalculator.CostListAdjusted), new[] {typeof(BuildableDef), typeof(ThingDef), typeof(bool)}),
+                new HarmonyMethod(typeof(ArchitectMod), nameof(AdjustStuff)));
+            harm.Patch(AccessTools.PropertyGetter(typeof(Designator_Build), nameof(Designator_Build.Visible)),
+                postfix: new HarmonyMethod(typeof(ArchitectMod), nameof(RequireGodMode)));
+            harm.Patch(AccessTools.Method(typeof(ListerThings), nameof(ListerThings.ThingsOfDef)), new HarmonyMethod(typeof(ArchitectMod), nameof(AdjustCount)));
+            harm.Patch(AccessTools.Method(typeof(ResourceCounter), nameof(ResourceCounter.GetCount), new[] {typeof(ThingDef)}),
+                new HarmonyMethod(typeof(ArchitectMod), nameof(AdjustCount)));
+        }
+
+        public static void AdjustCount(ref ThingDef __0)
+        {
+            if (__0 != null && __0.HasModExtension<StuffExtension_Cost>()) __0 = __0.GetModExtension<StuffExtension_Cost>().thingDef;
+        }
+
+        public static void RequireGodMode(ref bool __result, Designator_Build __instance)
+        {
+            if (__result && GetEntDef.Invoke(__instance).HasModExtension<BuildExtension_RequireGodMode>() && !DebugSettings.godMode) __result = false;
+        }
+
+        public static void AdjustStuff(ref ThingDef stuff)
+        {
+            if (stuff != null && stuff.HasModExtension<StuffExtension_Cost>()) stuff = stuff.GetModExtension<StuffExtension_Cost>().thingDef;
         }
 
         public static void IsVanillaBridge(IntVec3 c, TerrainGrid terrGrid, ref bool __result)
         {
             if (__result && terrGrid.TerrainAt(c).HasModExtension<TerrainExtension_CustomBridgeProps>()) __result = false;
         }
+    }
+
+    public class StuffExtension_Cost : DefModExtension
+    {
+        public ThingDef thingDef;
+    }
+
+    public class BuildExtension_RequireGodMode : DefModExtension
+    {
     }
 }
---
+++ 1.3/Source/VFEArchitect/Designator_RemoveFoundation.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace VFEArchitect
+{
+    public class Designator_RemoveFoundation : Designator_RemoveBridge
+    {
+        public Designator_RemoveFoundation()
+        {
+            defaultLabel = "VFEArchitect.DesignatorRemoveFoundation".Translate();
+            defaultDesc = "VFEArchitect.DesignatorRemoveFoundationDesc".Translate();
+            icon = ContentFinder<Texture2D>.Get("UI/Designators/RemoveBridge");
+            hotKey = KeyBindingDefOf.Misc5;
+        }
+
+        public override AcceptanceReport CanDesignateCell(IntVec3 c)
+        {
+            if (c.InBounds(Map) && !c.GetTerrain(Map).HasModExtension<TerrainExtension_Foundation>()) return false;
+
+            return base.CanDesignateCell(c);
+        }
+    }
+
+    public class TerrainExtension_Foundation : DefModExtension
+    {
+    }
+}
